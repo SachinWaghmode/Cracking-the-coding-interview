ca36cb87e874183bf38d48bb57346236bd4359dc
SachinWaghmode <sachinwaghmode57@gmail.com>
2017-11-03 23:55:08 -0700

    Create stringCompression.java

---
+++ stringCompression.java
@@ -0,0 +1,30 @@
+	import java.util.Scanner;
+
+	public class stringCompression {
+		public static void main(String[] args) {
+			Scanner scan = new Scanner(System.in);
+			System.out.println("\n Enter a string");
+		    String str1  = scan.next();
+		    System.out.println(stringCompression(str1));
+		    }
+		
+		private static String stringCompression(String str1){
+			int i=0, count=1;
+			StringBuilder result = new StringBuilder();
+			while(i< str1.length()-1){
+				if(str1.charAt(i) == str1.charAt(i+1)){
+					count++;
+				}
+				else{
+					result.append(str1.charAt(i));
+					result.append(count);
+					count=1;
+				}
+				i++;
+			}
+			result.append(str1.charAt(i));
+			result.append(count);
+			return str1.length() <= result.length() ?  str1 : result.toString() ;					
+		}
+
+	}
